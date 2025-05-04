d027eed38ddf4a40291336d3d56f17b889bac75b
pinkcrate <b.diaz.1@estudiantec.cr>
2025-05-04 16:16:41 -0600

    query ej 4 con sp recompile

---
+++ Queries/query4sp_recompile.sql
@@ -0,0 +1,15 @@
+-- 4. Uso de sp_recompile, cómo podría estar recompilando todos los SP existentes cada cierto tiempo?
+
+USE solturaDB;
+GO
+
+DECLARE @sql NVARCHAR(MAX) = '';
+
+SELECT @sql = @sql + 'EXEC sp_recompile ''' + SCHEMA_NAME(schema_id) + '.' + name + ''';' + CHAR(13)
+FROM solturaDB.sys.procedures;
+
+PRINT ' Script para recompilar todos los SPs:';
+PRINT @sql;
+
+EXEC sp_executesql @sql; -- Descomentar a su propio riesgo
+GO
