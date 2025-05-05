7a0f266d5b62db6710cabfa6d7aa652bccde9769
pinkcrate <b.diaz.1@estudiantec.cr>
2025-05-04 18:07:33 -0600

    query 9 avg
    Co-Authored-By: CGarcia1411 <169623334+CGarcia1411@users.noreply.github.com>

---
+++ Queries/query7y8substringLtrim.sql
@@ -0,0 +1,20 @@
+--7 SUBSTRING para extraer partes de descripciones.
+--8 LTRIM para limpiar strings.
+USE solturaDB;
+GO
+SELECT 
+    planID,
+    description AS original_desc,
+    LTRIM(description) AS cleaned_desc,
+    SUBSTRING(LTRIM(description), 1, 20) AS short_desc,
+    CASE 
+        WHEN CHARINDEX('-', description) > 0 
+        THEN SUBSTRING(description, CHARINDEX('-', description) + 1, LEN(description))
+        WHEN CHARINDEX('(', description) > 0 
+        THEN SUBSTRING(description, CHARINDEX('(', description), CHARINDEX(')', description) - CHARINDEX('(', description) + 1)
+        ELSE ''
+    END AS category_info
+FROM solturaDB.sol_plans;
+GO
+
+select * from solturaDB.sol_plans
