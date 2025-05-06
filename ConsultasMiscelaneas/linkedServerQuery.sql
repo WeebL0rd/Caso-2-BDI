1e950365d6e32dec46fe6a150bad9b752b4e2e96
CGarcia1411 <carlos.andres.garcia.molina.2005@gmail.com>
2025-05-06 17:10:39 -0600

    Create linkedServerQuery.sql

---
+++ ConsultasMiscelaneas/linkedServerQuery.sql
@@ -0,0 +1,111 @@
+USE master;
+GO
+
+-- Verifica si el Linked Server ya existe. Si existe, lo elimina.
+IF EXISTS (SELECT 1 FROM sys.servers WHERE name = 'LocalServer')
+BEGIN
+    PRINT 'Eliminando el Linked Server existente...';
+    EXEC sp_dropserver @server = 'LocalServer', @droplogins = 'droplogins';
+END;
+GO
+
+--Se crea un servidor vincluado a una estancia local, esto puesto no tenemos un servidor real. 
+EXEC sp_addlinkedserver
+    @server = 'LocalServer', 
+    @srvproduct = 'SQL Server';
+
+EXEC sp_addlinkedsrvlogin
+    @rmtsrvname = 'LocalServer',
+    @useself = 'True',
+    @locallogin = NULL;
+
+EXEC sp_serveroption 'LocalServer', 'rpc', 'TRUE';
+EXEC sp_serveroption 'LocalServer', 'rpc out', 'TRUE';
+GO
+
+PRINT 'Servidor vinculado.';
+
+
+
+--Se crea una tabla de bitácora en esta instancia local. 
+USE master;
+GO
+
+IF NOT EXISTS (SELECT 1 FROM sys.tables WHERE name = 'RegistroBitacora')
+BEGIN
+    CREATE TABLE  dbo.RegistroBitacora (
+        ID INT PRIMARY KEY IDENTITY(1,1),
+        FechaHora DATETIME,
+        NombreProcedimiento NVARCHAR(255),
+        Mensaje NVARCHAR(MAX),
+        UsuarioSQL NVARCHAR(255),
+        Servidor NVARCHAR(255),
+        Gravedad INT NOT NULL,
+        NombreAplicacion NVARCHAR(255) );
+    PRINT 'Tabla de bitácora creada.';
+END;
+ELSE
+    PRINT 'La tabla de bitácora ya existe.';
+GO
+
+
+-- Utilizando la base de datos, se hace un proceso de almacenamiento de datos.
+
+USE solturaDB;
+GO
+
+IF OBJECT_ID('sp_RegistrarEventoBitacora', 'P') IS NOT NULL
+BEGIN
+    PRINT 'Eliminando el procedimiento almacenado existente...';
+    DROP PROCEDURE sp_RegistrarEventoBitacora;
+END;
+GO
+
+CREATE PROCEDURE sp_RegistrarEventoBitacora (
+    @NombreProcedimiento NVARCHAR(255),
+    @Mensaje NVARCHAR(MAX),
+    @Gravedad INT = 0,
+    @NombreAplicacion NVARCHAR(255) = NULL
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    BEGIN TRY
+        -- Inserta en la tabla de bitácora del servidor en la instancia local!!!!!
+        INSERT INTO master.dbo.RegistroBitacora (
+            FechaHora,
+            NombreProcedimiento,
+            Mensaje,
+            UsuarioSQL,
+            Servidor,
+            Gravedad,
+            NombreAplicacion
+        )
+        VALUES ( GETDATE(),@NombreProcedimiento, @Mensaje, SUSER_SNAME(),@@SERVERNAME, @Gravedad,
+				ISNULL(@NombreAplicacion, APP_NAME()));
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error al registrar en la bitácora: ' + ERROR_MESSAGE();
+    END CATCH
+END;
+GO
+
+PRINT 'Procedimiento almacenado genérico creado exitosamente.';
+
+
+
+
+USE solturaDB;
+GO
+
+--Proceso para probar el Script
+EXEC sp_RegistrarEventoBitacora
+    @NombreProcedimiento = 'PruebaDeBitacora',
+    @Mensaje = 'Esto es un mensaje de prueba.',
+    @Gravedad = 0,
+    @NombreAplicacion = 'Soltura';
+
+-- Verifica el registro en la tabla de bitácora
+SELECT * FROM master.dbo.RegistroBitacora;
+GO
