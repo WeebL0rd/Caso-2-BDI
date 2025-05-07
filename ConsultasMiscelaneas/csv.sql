da9686434c9cd86421fcd1e6cee1927e58a91e34
RachellLeiva <r.leiva.1@estudiantec.cr>
2025-05-06 21:12:43 -0600

    Update csv.sql

--- ConsultasMiscelaneas/csv.sql
+++ ConsultasMiscelaneas/csv.sql
@@ -1,10 +1,12 @@
-Use solturaDB
--- Primero, esta es la consulta con JOIN
-SELECT 
-    u.userID, 
+USE solturaDB;
+GO
+
+SELECT
+    u.userID,
     u.firstName + ' ' + u.lastName AS fullName,
-    u.email, 
-    up.adquisition, 
+    u.email,
+    up.adquisition,
     up.enabled
-FROM solturaDB.sol_users u
-JOIN solturaDB.sol_userPlans up ON u.userID = up.userID;
+FROM solturaDB.sol_users      AS u
+JOIN solturaDB.sol_userPlans AS up
+  ON u.userID = up.userID;
